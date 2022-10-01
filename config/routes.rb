b707984691438b364ad1b354ccfd87b1aa796c4b
EC2 Default User <ec2-user@ip-172-31-33-75.ec2.internal>
2022-10-01 21:02:42 +0000

    6syou

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
 
   resources :books
 
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # get 'homes/top'
   get '/' => 'homes#top'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: 'home#top'
   # resources :
